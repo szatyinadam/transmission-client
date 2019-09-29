9b289c724320cb8146ebbdfb4990986c19252358
Adam Szatyin <szatyinadam@gmail.com>
2019-09-29 23:36:26 +0200

    Add message consuming and deletion

--- main.go
+++ main.go
@@ -1,13 +1,44 @@
 package main
 
-import "log"
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/sqs"
+	"github.com/aws/aws-sdk-go/service/sqs/sqsiface"
+	"log"
+)
 
 func main() {
 	config := ReadConfig()
-	torrents := GetTorrents(&config.Transmission)
+	sqsService := createSqsService(&config.Sqs)
+	messageChannel := make(chan *sqs.Message, config.Sqs.MaxMessages)
 
-	for index, name := range torrents {
-		log.Printf("%d %s", index, name)
+	go PollSqs(messageChannel, sqsService, &config.Sqs)
+	processMessage(messageChannel, config, sqsService)
+
+	log.Printf("Listening on queue: %s", config.Sqs.QueueUrl)
+}
+
+func processMessage(messageChannel chan *sqs.Message, config *Config, sqsService sqsiface.SQSAPI) {
+	for message := range messageChannel {
+		log.Println(message)
+		for index, name := range GetTorrents(&config.Transmission) {
+			log.Printf("%d %s", index, name)
+		}
+		DeleteReceivedMessages(sqsService, &config.Sqs, message)
 	}
-	Consume(&config.Sqs)
+}
+
+func createSqsService(sqsConfig *Sqs) sqsiface.SQSAPI {
+	sess, err := session.NewSession(&aws.Config{
+		Region:      aws.String(sqsConfig.Region),
+		Credentials: credentials.NewSharedCredentials(sqsConfig.CredentialPath, sqsConfig.CredentialProfile),
+		MaxRetries:  aws.Int(5),
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	svc := sqs.New(sess)
+	return svc
 }
--- messageconsumer.go
+++ messageconsumer.go
@@ -2,55 +2,40 @@ package main
 
 import (
 	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/credentials"
-	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/sqs"
+	"github.com/aws/aws-sdk-go/service/sqs/sqsiface"
 	"log"
 )
 
-func Consume(sqsConfig *Sqs) {
-	svc := createSqs(sqsConfig)
+func PollSqs(chanel chan<- *sqs.Message, sqsService sqsiface.SQSAPI, sqsConfig *Sqs) {
 	receiveParams := &sqs.ReceiveMessageInput{
-		QueueUrl:            aws.String(sqsConfig.QueueUrl),
-		MaxNumberOfMessages: aws.Int64(3),
-		VisibilityTimeout:   aws.Int64(30),
+		QueueUrl:            &sqsConfig.QueueUrl,
+		MaxNumberOfMessages: aws.Int64(sqsConfig.MaxMessages),
 		WaitTimeSeconds:     aws.Int64(20),
 	}
 	for {
-		receiveResp, err := svc.ReceiveMessage(receiveParams)
-		if err != nil {
-			log.Println(err)
-		}
-		log.Printf("Received message - %s", receiveResp)
-		if receiveResp != nil {
-			deleteReceivedMessages(svc, sqsConfig, receiveResp)
-		}
+		Receive(sqsService, receiveParams, chanel)
 	}
 }
 
-func createSqs(sqsConfig *Sqs) *sqs.SQS {
-	sess, err := session.NewSession(&aws.Config{
-		Region:      aws.String(sqsConfig.Region),
-		Credentials: credentials.NewSharedCredentials(sqsConfig.CredentialPath, sqsConfig.CredentialProfile),
-		MaxRetries:  aws.Int(5),
-	})
+func Receive(sqsService sqsiface.SQSAPI, receiveParams *sqs.ReceiveMessageInput, chanel chan<- *sqs.Message) {
+	output, err := sqsService.ReceiveMessage(receiveParams)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Failed to fetch SQS message %v", err)
 	}
-	svc := sqs.New(sess)
-	return svc
+	for _, message := range output.Messages {
+		chanel <- message
+	}
 }
 
-func deleteReceivedMessages(svc *sqs.SQS, sqsConfig *Sqs, receivedMessages *sqs.ReceiveMessageOutput) {
-	for _, message := range receivedMessages.Messages {
-		deleteParams := &sqs.DeleteMessageInput{
-			QueueUrl:      aws.String(sqsConfig.QueueUrl),
-			ReceiptHandle: message.ReceiptHandle,
-		}
-		_, err := svc.DeleteMessage(deleteParams)
-		if err != nil {
-			log.Println(err)
-		}
-		log.Printf("Delete message - Message ID: %s has beed deleted.", *message.MessageId)
+func DeleteReceivedMessages(sqsService sqsiface.SQSAPI, sqsConfig *Sqs, message *sqs.Message) {
+	deleteParams := &sqs.DeleteMessageInput{
+		QueueUrl:      aws.String(sqsConfig.QueueUrl),
+		ReceiptHandle: message.ReceiptHandle,
 	}
+	_, err := sqsService.DeleteMessage(deleteParams)
+	if err != nil {
+		log.Println(err)
+	}
+	log.Printf("Delete message - Message ID: %s has beed deleted.", *message.MessageId)
 }
